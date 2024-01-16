be72cf038ca97e6e16043acd030f9d84da610f21
tushar mahat <tusharmahat@gmail.com>
2024-01-15 22:08:01 -0700

    update email-system link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,7 @@ function Projects() {
         window.location.href = "https://github.com/tusharmahat/hackernews-pagination";
     }
     const openEmailDemo = () => {
-        window.location.href = "https://dark-tan-sockeye-cuff.cyclic.cloud";
+        window.location.href = "https://email-system.onrender.com";
     }
     const openEmailGithub = () => {
         window.location.href = "https://github.com/tusharmahat/email-system";
