3f9293e5fb5733601c077b3fce9202c74d97bb81
tushar mahat <tusharmahat@gmail.com>
2024-02-25 22:23:32 -0700

    fix link

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,8 +1,11 @@
 import '../App.css';
-import linkedIn from "../images/linkedin.png"
+// import linkedIn from "../images/linkedin.png"
 import gitHub from "../images/github.png"
 function Social() {
 
+    const openGitHub = () => {
+        window.location.href = 'https://github.com/tusharmahat';
+    }
     return (
         <div id="social-container">
             {/* <button className="btn-holder">   <img src={linkedIn} alt="linkedin" className='icon' id="linkedin_icon" onClick={openLinkedIn} onDragStart={(event) => event.preventDefault()} /></button> */}
